a053f8abd6c2d0283de14c4068283f60bf30afa4
haseebrayan <gulfamhaseebi@gmail.com>
2025-11-08 17:40:09 +0500

    added png and cs files

---
+++ ChangeMusicVolume.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ChangeMusicVolume : MonoBehaviour {
+
+	// Use this for initialization
+
+	public Slider Volume;
+	public AudioSource MyMusic;
+
+
+	// Update is called once per frame 1
+
+	void update () {
+	
+		MyMusic.volume = Volume.value;
+	
+	}
+
+
+}
